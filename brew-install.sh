5e4f0f4086c1851c6e4249acc0431e4b28c5c1b5
wangjp <wangjp@rd.netease.com>
2022-09-24 23:52:47 +0800

    add brew-install

---
+++ brew-install.sh
@@ -0,0 +1,35 @@
+brew install tmux
+brew install neovim
+brew install java
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install java
+brew install neovim
+brew install node
+brew install autojump
+brew install expect
+brew install tsh
+brew install teleport
+brew install gauth
+brew install go
+brew install cmake
+brew install z
+brew install bat
+brew install tree
+brew install rg
+brew install fd
+brew uninstall tmux
+brew tap
+brew tap-new
+brew tap-new $USER/local-tmux
+brew extract --version=3.1b tmux $USER/local-tmux
+brew install tmux@3.1b
+brew install fd
+brew install cloc
+brew install gdb
+brew install --cask linearmouse
+brew install fzf
+brew install ripgrep
+$(brew --prefix)/opt/fzf/install
+brew install --cask bob
+brew install coreutils findutils
+brew install ctags
